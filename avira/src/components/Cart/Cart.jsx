b958720cc02a3f1c9e7be5c50f9b4e3d1b70914e
matt7861 <114481119+matt7861@users.noreply.github.com>
2023-04-04 19:57:28 +1000

    added stripe payments

--- avira/src/components/Cart/Cart.jsx
+++ avira/src/components/Cart/Cart.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removeItem, resetCart } from "../../redux/cartReducer";
 import "./Cart.scss";
+import { loadStripe } from "@stripe/stripe-js";
+import { makeRequest } from "../../makeRequest";
 
 export const Cart = () => {
   const products = useSelector((state) => state.cart.products);
@@ -11,6 +13,24 @@ export const Cart = () => {
     return total.toFixed(2);
   };
   const dispatch = useDispatch();
+
+  const stripePromise = loadStripe(
+    "pk_test_51MqvOjDpT19KJ4yCT15o04bX51wtDefqVjFPzUJEWqd8uGohru5WbrGYrWPqJq5TJi0igzlMyVp3fr5pBMGfVDNC00JeO8TZag"
+  );
+
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="cart">
@@ -23,7 +43,7 @@ export const Cart = () => {
         </div>
       ))}
       <div>$ {totalPrice()}</div>
-
+      <button onClick={handlePayment}>Checkout</button>
       <button onClick={() => dispatch(resetCart())}>empty</button>
     </div>
   );
